ec0d58a93d8f8474647bf1f48f2476e2866ce8f5
Артём Гапеевцев <a.gapeevtsev@samolet.ru>
2024-01-21 16:06:03 +0800

    Add comments

--- Seminar5/MVCStudentApp/src/Controller/Interfaces/iGetController.java
+++ Seminar5/MVCStudentApp/src/Controller/Interfaces/iGetController.java
@@ -1,6 +1,35 @@
 package Controller.Interfaces;
 
+import Controller.ViewException;
+
 public interface iGetController {
+    /**
+     * Установка текущего отображения
+     * @param language
+     * @throws ViewException
+     */
+    public void setView(String language) throws ViewException;
+    /**
+     * Добавление нового типа отображения
+     * @param language
+     * @param view
+     */
+    public void addView(String language, iGetView view);
+    /**
+     * Лобавление нового типа модели
+     * @param model
+     */
+    public void addModel(iGetModel model);
+    /**
+     * Выбор языка
+     * @throws ViewException
+     */
+    public void selectLanguage() throws ViewException;
+    /**
+     * Обновление контроллера
+     * @param Request
+     */
+    public void update(String Request);
     /**
      * Запуск контроллера
      */
